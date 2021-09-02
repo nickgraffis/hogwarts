4fcf224d1ddf220aac1903b19709dccc0c55d14e
Nick Graffis <nicholasgraffis@gmail.com>
2021-09-02 12:10:47 -0700

    proof of concept, faunadb permissions

--- functions/hello-world/hello-world.ts
+++ functions/hello-world/hello-world.ts
@@ -1,23 +1,56 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 import { Handler } from '@netlify/functions';
+import { Call, Client, Index, Login, Match, Time } from 'faunadb';
+const { FAUNADB: secret } = process.env;
+import safeAwait from '../../safeAwait';
 
 const getRandomInt = (max: number): number => {
   return Math.floor(Math.random() * max);
 }
 
-const handler: Handler = async (event) => {
-  try {
-    const names: string[] = ['Rick', 'Morty', 'Summer', 'Gerry', 'Korvo', 'Terry']
-    const subject: string = event.queryStringParameters?.name || names[getRandomInt(names.length)]
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello from Netlify, ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: JSON.stringify({message: error.toString()}) }
+const handler: Handler = async (event, context) => {
+  if (event.httpMethod !== 'POST') return {
+    statusCode: 403,
+    body: "Invalid HTTP Method. POST requests only."
+  }
+
+  if (!event.body) return {
+    statusCode: 403,
+    body: "Please provide credentials inside the body of request!"
+  }
+
+  if (!secret) return {
+    statusCode: 500,
+    body: "Internal Server Error: No FaunaDB server secret is set."
+  }
+
+  const client = new Client({ secret })
+
+  //Display the date 1 hour from now
+  const oneHourFromNow = () => {
+    const now = new Date();
+    now.setHours(now.getHours() + 1);
+    return now.toISOString();
+  }
+
+  const { wand, password } = JSON.parse(event.body)
+
+  const [error, token] = await safeAwait(
+    client.query(
+      Call("LoginAccount", wand, password)
+    )
+  )
+
+  console.log(error)
+
+  if (error) return {
+    statusCode: parseInt(error.requestResult.statusCode),
+    body: JSON.stringify(error.description)
+  }
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify(token)
   }
 }
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,78 @@
+import { Client, Map, Paginate, Match, Index, Lambda, Get, Var, CurrentIdentity, CurrentToken, Collection, Ref } from 'faunadb'
 import React, { FC, useState } from 'react'
-import logo from './logo.svg'
+import { useEffect } from 'react'
+import safeAwait from '../safeAwait'
 
 type Props = { }
 
 export const App: FC<Props> = () => {
-  const [count, setCount] = useState<number>(0)
-  const [netlify, setNetlify] = useState<string | undefined>(undefined)
-  const [input, setInput] = useState<string>('')
+  const [key, setKey] = useState<string | null>(null)
+  const [error, setError] = useState<string | null>(null)
+  const [conversations, setConversations] = useState<string[]>([])
 
-  const handleInput = (event: React.KeyboardEvent): void => {
-    if (event.key === 'Enter') fetchFromNetlify(input)
-  }
-
-  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setInput(event.target.value)
-  }
-
-  const fetchFromNetlify = (name?: string): void => {
-    const endpoint: string = '/api/hello-world' + (name ? '?name=' + name : '')
-    fetch(endpoint)
-    .then(
-      (res: any) => {
-        res.json().then((res: any) => setNetlify(res.message))
+  useEffect(() => {
+    fetch('/api/hello-world', {
+      method: 'POST',
+      body: JSON.stringify({
+        wand: "holly-phoenix",
+        password: "harrypotter"
+      })
+    })
+    .then(res => {
+      if (!res.ok) {
+        console.log(res)
+        throw Error(res);
       }
+      return res;
+    })
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+      setKey(res.tokens.access.secret)
+      console.log(res.tokens.access.secret)
+    })
+    .catch(err => {
+      console.log(err)
+      setError(err.toString())
+    })
+  }, [])
+
+  const fetchConversations = async (key: string) => {
+    const client = new Client({
+      secret: key
+    })
+    const [error, data] = await safeAwait(
+      client.query(
+        Map(
+          Paginate(
+            Match(Index("all_conversations"))
+          ), 
+          Lambda(
+            'conversation', Get(Var('conversation'))
+          )
+        )
+      )
     )
+
+    if (error) console.log(error)
+    if (data) setConversations(data.data.map((c: any) => c.data.message))
+    console.log(data)
   }
+
+  useEffect(() => {
+    key && fetchConversations(key)
+  }, [key])
 
   return (
     <div className="text-center">
-      <header className="bg-[#282c34] min-h-screen flex flex-col items-center justify-center space-y-4 text-2xl text-white">
-        <img src={logo} className="h-[40vmin] animate-spinslow" alt="logo" />
-        <p>Hello Vite + React + Typescript + Netlify + TailwindCSS!</p>
-        <p>
-          <button className="bg-blueGray-400 p-2 rounded-md focus:outline-none" 
-          onClick={() => setCount((count) => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          <button className="bg-blueGray-400 p-2 rounded-md focus:outline-none"
-          onClick={() => fetchFromNetlify()}
-          >
-            Try Netlify
-          </button>
-        </p>
-        <p>Netlify Response from <code className="text-lime-400">/api/hello-world </code>:
-          <span className="text-[#61dafb]"> { netlify } </span>
-        </p>
-        <p className="space-y-2">
-          <p>Try Netlify with query string paramter. Enter name and press enter</p>
-          <input 
-          onKeyUp={handleInput} 
-          onChange={handleChangeInput}
-          value={input}
-          className="apperance-none bg-[#282c34] rounded-md border-blueGray-400 border-2 px-2 py-1" placeholder="What's Your Name?" 
-          />
-        </p>
-        <p>Netlify Response from <code className="text-lime-400">/api/hello-world?name= </code>:
-         <span className="text-[#61dafb]"> { netlify } </span>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="text-[#61dafb]"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="text-[#61dafb]"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-          {' | '}
-          <a
-            className="text-[#61dafb]"
-            href="https://docs.netlify.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Netlify Docs
-          </a>
-        </p>
-      </header>
+      { key ? 
+        <p className="font-bold text-xl">{ key }</p> :
+        <p className="font-bold text-xl">{ 'Not Logged In' }</p>
+      }
+      { error && <p className="font-bold text-red-400 text-xl">{ error }</p> }
+      <ul>
+        { conversations.map(conversation => <li key={ conversation }>{ conversation }</li>) }
+      </ul>
     </div>
   )
 }
